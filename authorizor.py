40a395202344acacbfe6f47cef1043d7bc21fcda
Norman Oliden <naoliden@uc.cl>
2017-07-25 21:07:07 -0400

    Tercer commit con el programa completo y supuestamente funcionasupuestamente l

--- authorizor.py
+++ authorizor.py
@@ -26,6 +26,32 @@ class Authorizor:
                 raise InvalidUsername(username)
             perm_set.add(username)
 
+    def check_permission(self, prem_name, username):
+        if not self.authenticator.is_logged_in(username):
+            raise NotLoggedInError(username)
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            raise PermissionError("Permission does not exist")
+        else:
+            if username not in perm_set:
+                raise NotPermittedError(username)
+            else:
+                return True
+
 
 class PermissionError(Exception):
     pass
+
+
+class NotLoggedInError(Exception):
+    pass
+
+
+class NotPermittedError(Exception):
+    pass
+
+
+if __name__ == "__main__":
+
+    authorizor = Authorizor(authenticator)
