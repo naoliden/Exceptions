7baec10957d7f6473193d4230fc588bd072d0387
Norman Oliden <naoliden@uc.cl>
2017-07-25 23:51:56 -0400

    Modulo auth.py terminado

--- auth.py
+++ auth.py
@@ -3,12 +3,15 @@ import hashlib
 
 class User:
     def __init__(self, username, password):
-        self.password = self.encriptar_pw(password)
+        self.username = username
+        self.password = str(self._encriptar_pw(password))
+        # self.password = (password)
+
         self.is_logged_in = False
 
     def _encriptar_pw(self, password):
         '''Encripta el password con el username y retorna el sha digest'''
-        hash_string = self.username + self.password
+        hash_string = str(self.username) + str(password)
         hash_string = hash_string.encode('utf8')
         return hashlib.sha256(hash_string).hexdigest()
 
@@ -17,34 +20,10 @@ class User:
         return encrypted == self.password
 
 
-class AuthException(Exception):
-    def __init__(self, username, user=None):
-        ''' El primer parametro es un nuombre de usuario y el segundo es el objeto
-         User asociado a este nombre.'''
-        super().__init__(username, user)
-        self.username = username
-        self.user = user
-
-
-class UsernameAlreadyExists(AuthException):
-    pass
-
-
-class PasswordTooShort(AuthException):
-    pass
-
-
-class InvalidUsername(AuthException):
-    pass
-
-
-class InvalidPassword(AuthException):
-    pass
-
-
 class Authenticator:
+
     def __init__(self):
-        self.users = []
+        self.users = {}
 
     def add_users(self, username, password):
         if username in self.users:
@@ -74,5 +53,85 @@ class Authenticator:
             return False
 
 
-# if __name__ == '__main__':
+class Authorizor:
+
+    def __init__(self, authenticator):
+        self.authenticator = authenticator
+        self.permissions = {}
+
+    def add_permission(self, perm_name):
+        '''crea permisos a los que se pueden agregar usuarios
+        user --- > permiso'''
+
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            self.permissions[perm_name] = set()
+        else:
+            raise PermissionError("Permission Exists")
+
+    def permit_user(self, perm_name, username):
+        ''' Otorga el permiso x recibido como input al usuario y'''
+
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            raise PermissionError("Permission does not exist")
+        else:
+            if username not in self.authenticator.users:
+                raise InvalidUsername(username)
+            perm_set.add(username)
+
+    def check_permission(self, perm_name, username):
+        if not self.authenticator.is_logged_in(username):
+            raise NotLoggedInError(username)
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            raise PermissionError("Permission does not exist")
+        else:
+            if username not in perm_set:
+                raise NotPermittedError(username)
+            else:
+                return True
+
+
+class AuthException(Exception):
+    def __init__(self, username, user=None):
+        ''' El primer parametro es un nuombre de usuario y el segundo es el objeto
+         User asociado a este nombre.'''
+        super().__init__(username, user)
+        self.username = username
+        self.user = user
+
+
+class UsernameAlreadyExists(AuthException):
+    pass
+
+
+class PasswordTooShort(AuthException):
+    pass
+
+
+class InvalidUsername(AuthException):
+    pass
+
+
+class InvalidPassword(AuthException):
+    pass
+
+
+class PermissionError(Exception):
+    pass
+
+
+class NotLoggedInError(Exception):
+    pass
+
+
+class NotPermittedError(Exception):
+    pass
+
+
 authenticator = Authenticator()
+authorizor = Authorizor(authenticator)
